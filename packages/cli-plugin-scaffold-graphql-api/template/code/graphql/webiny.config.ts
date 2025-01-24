0581a6e48ce328e3a5f50216745bccc4afb8e1be
Bruno Zorić <bruno.zoric@gmail.com>
2025-01-24 15:54:41 +0100

    fix: remove old packages (#4504)

--- packages/cli-plugin-scaffold-graphql-api/template/code/graphql/webiny.config.ts
+++
@@ -1,8 +0,0 @@
-const { createWatchFunction, createBuildFunction } = require("@webiny/project-utils");
-
-module.exports = {
-    commands: {
-        watch: createWatchFunction({ cwd: __dirname }),
-        build: createBuildFunction({ cwd: __dirname })
-    }
-};
